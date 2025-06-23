33942cd9070819fce9d024804ecd7ba9d03bb2d3
mshariqazeem <msa200007@utdallas.edu>
2025-06-22 19:24:44 -0500

    Updated the models

--- restaurant/models.py
+++ restaurant/models.py
@@ -3,19 +3,19 @@ from django.db import models
 
 # Create your models here.
 class Booking(models.Model):
-    first_name = models.CharField(max_length=200)
-    reservation_date = models.DateField()
-    reservation_slot = models.SmallIntegerField(default=10)
+    name = models.CharField(max_length = 255, null=False)
+    no_of_guests = models.IntegerField()
+    reservation_date = models.DateTimeField()
 
-    def __str__(self): 
-        return self.first_name
+    def __str__(self):
+        return self.name
 
 
 # Add code to create Menu model
 class Menu(models.Model):
-   name = models.CharField(max_length=200) 
-   price = models.IntegerField(null=False) 
-   menu_item_description = models.TextField(max_length=1000, default='') 
+   title = models.CharField(max_length = 255)
+   price = models.DecimalField(max_digits = 10, decimal_places = 2)
+   inventory = models.SmallIntegerField()
 
    def __str__(self):
-      return self.name
+      return self.title
--- restaurant/views.py
+++ restaurant/views.py
@@ -9,7 +9,6 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
-
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
@@ -32,19 +31,17 @@ def book(request):
     context = {'form':form}
     return render(request, 'book.html', context)
 
-# Add your code here to create new views
 def menu(request):
     menu_data = Menu.objects.all()
     main_data = {"menu": menu_data}
     return render(request, 'menu.html', {"menu": main_data})
 
-
-def display_menu_item(request, pk=None): 
-    if pk: 
-        menu_item = Menu.objects.get(pk=pk) 
-    else: 
-        menu_item = "" 
-    return render(request, 'menu_item.html', {"menu_item": menu_item}) 
+def display_menu_item(request, pk=None):
+    if pk:
+        menu_item = Menu.objects.get(pk=pk)
+    else:
+        menu_item = ""
+    return render(request, 'menu_item.html', {"menu_item": menu_item})
 
 @csrf_exempt
 def bookings(request):
